54bea36c8dda6559435f7f8e22f8e7bcdac4385e
inkcherry <mingzhi.liu@intel.com>
2023-12-12 06:26:39 +0000

    fix bwd err

--- megatron/model/rotary_pos_embedding.py
+++ megatron/model/rotary_pos_embedding.py
@@ -6,7 +6,7 @@
 
 import importlib.util
 import torch
-
+from deepspeed import get_accelerator
 from torch import einsum, nn
 
 __all__ = ['RotaryEmbedding', 'apply_rotary_pos_emb']
@@ -14,7 +14,7 @@ __all__ = ['RotaryEmbedding', 'apply_rotary_pos_emb']
 class RotaryEmbedding(nn.Module):
     def __init__(self, dim):
         super().__init__()
-        inv_freq = 1.0 / (10000 ** (torch.arange(0, dim, 2).float() / dim))
+        inv_freq = 1.0 / (10000 ** (torch.arange(0, dim, 2).float() / dim)).to(get_accelerator().current_device_name())
         self.register_buffer('inv_freq', inv_freq)
         if importlib.util.find_spec('einops') is None:
             raise RuntimeError("einops is required for Rotary Embedding")
@@ -22,13 +22,16 @@ class RotaryEmbedding(nn.Module):
     def forward(self, max_seq_len, offset=0):
         seq = torch.arange(max_seq_len, device=self.inv_freq.device) + offset
         freqs = einsum('i , j -> i j', seq.type_as(self.inv_freq), self.inv_freq)
+        #[512 64]
         # first part even vector components, second part odd vector components,
         #  2 * dim in dimension size
         emb = torch.cat((freqs, freqs), dim=-1)
+        #[512,128]
         # emb [seq_length, .., dim]
         from einops import rearrange
-        return rearrange(emb, 'n d -> n 1 1 d')
-
+        tmp = rearrange(emb, 'n d -> n 1 1 d')
+        # print("tmp",tmp.shape)
+        return tmp
 
 def _rotate_half(x):
     """
@@ -37,6 +40,7 @@ def _rotate_half(x):
     from einops import rearrange
     x = rearrange(x, '... (j d) -> ... j d', j=2)
     x1, x2 = x.unbind(dim=-2)
+    # print("x1,x2,", x.shape,x1.shape,x2.shape)
     return torch.cat((-x2, x1), dim=-1)
 
 
@@ -49,8 +53,12 @@ def apply_rotary_pos_emb(t, freqs):
     rot_dim = freqs.shape[-1]
     # ideally t_pass is empty so rotary pos embedding is applied to all tensor t
     t, t_pass = t[..., :rot_dim], t[..., rot_dim:]
+    # print("t,t_pass",t.shape, t_pass.shape)
 
     # first part is cosine component
     # second part is sine component, need to change signs with _rotate_half method
     t = (t * freqs.cos().to(t.dtype)) + (_rotate_half(t) * freqs.sin().to(t.dtype))
-    return torch.cat((t, t_pass), dim=-1)
+    # print(t.shape)
+    if t_pass.shape[-1]==0:
+        return t
+    # return torch.cat((t, t_pass), dim=-1)
